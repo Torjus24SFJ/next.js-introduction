d44d9426e067f73d70311b45294fc6e08263d8c4
Torjus <Torjus24sfj@gmail.com>
2025-02-05 12:41:40 +0100

    btn fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
+import { Button } from "./components/Button";
 
 export default function Home() {
   return (
    <section>
-    <h1>This is homepage</h1>
-    <div>
+    <h1 className="text-2xl font-bold text-center m-6">Home</h1>
+    <div className="flex flex-row gap-6 place-content-center m-6">
     <figure>
-      <Image width={300} height={300} className="" src="/heisendrated.png" alt="" />
+      <Image width={300} height={300} src="/heisendrated.png" alt="" />
     </figure>
     <figure>
-      <Image width={300} height={300} className="" src="/heisendrated.png" alt="" />
+      <Image width={300} height={300}  src="/heisendrated.png" alt="" />
     </figure>
+    <Button />
     </div>
    </section>
   );
